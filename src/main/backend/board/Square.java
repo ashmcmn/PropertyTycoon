d948d6382e37ecafdb9a993570658fc459cac755
ashley m <afm30@sussex.ac.uk>
2020-03-31 21:09:05 +0100

    Added board class and various square classes

---
+++ src/main/backend/board/Square.java
@@ -0,0 +1,37 @@
+package main.backend.board;
+
+/**
+ * The type Square.
+ *
+ * @author Ashley McManamon
+ */
+public class Square {
+    private String name;
+
+    /**
+     * Instantiates a new Square.
+     *
+     * @param name the name of the square
+     */
+    public Square(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
