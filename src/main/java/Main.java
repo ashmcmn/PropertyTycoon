2e7b360150303e434eddce1e34260a3a55f31689
Harry Pease <hwp20@sussex.ac.uk>
2020-05-11 19:29:29 +0100

    Added Menu GUI with buttons. No functionality added the the backend.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,10 +8,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("frontend/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        //Parent root = FXMLLoader.load(getClass().getResource("frontend/sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.show();
     }
 
 
--- src/main/java/frontend/menu/mainMenu.java
+++ src/main/java/frontend/menu/mainMenu.java
@@ -1,18 +1,21 @@
 package frontend.menu;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.image.*;
 
 import java.io.*;
+
+
 import javafx.geometry.*;
 import javafx.scene.Group;
 
@@ -26,25 +29,36 @@ public class mainMenu extends Application {
             primaryStage.setTitle("Property Tycoon - Start Menu");
 
             //Creating menu buttons and assigning icons
-            FileInputStream start = new FileInputStream("src/main/frontend/resources/start.png");
-            Image startIcon = new Image(start, 50, 50, false, false);
+            FileInputStream start = new FileInputStream("src/main/java/frontend/resources/start.png");
+            Image startIcon = new Image(start, 30, 30, false, false);
             ImageView iv1 = new ImageView(startIcon);
             Button btnStart = new Button("Start", iv1);
+            btnStart.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("not implimented yet ");
+                }
+            });
 
 
-            FileInputStream help = new FileInputStream("src/main/frontend/resources/help.png");
-            Image helpIcon = new Image(help, 50, 50, false, false);
+            FileInputStream help = new FileInputStream("src/main/java/frontend/resources/help.png");
+            Image helpIcon = new Image(help, 30, 30, false, false);
             ImageView iv2 = new ImageView(helpIcon);
             Button btnHelp = new Button("Help", iv2);
             btnHelp.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
+                    //WebView browser = new WebView();
+                    //WebEngine webEngine = browser.getEngine();
+                    //URL url = getClass().getResource("src/main/java/frontend/resources/website/userguide.html");
 
+                    //getHostServices().showDocument(url.toString());
                 }
             });
 
-            FileInputStream exit = new FileInputStream("src/main/frontend/resources/exit.png");
-            Image exitIcon = new Image(exit, 50, 50, false, false);
+            FileInputStream exit = new FileInputStream("src/main/java/frontend/resources/exit.png");
+            Image exitIcon = new Image(exit, 30, 30, false, false);
             ImageView iv3 = new ImageView(exitIcon);
             Button btnExit = new Button("Exit", iv3);
             btnExit.setOnAction(new EventHandler<ActionEvent>() {
@@ -54,8 +68,8 @@ public class mainMenu extends Application {
                 }
             });
 
-            FileInputStream settings = new FileInputStream("src/main/frontend/resources/settings.png");
-            Image settingsIcon = new Image(settings, 50, 50, false, false);
+            FileInputStream settings = new FileInputStream("src/main/java/frontend/resources/settings.png");
+            Image settingsIcon = new Image(settings, 30, 30, false, false);
             ImageView iv4 = new ImageView(settingsIcon);
             Button btnSettings = new Button("Settings", iv4);
             btnSettings.setOnAction(new EventHandler<ActionEvent>() {
@@ -73,55 +87,102 @@ public class mainMenu extends Application {
                 }
             });
 
+            //New Game Buttons
+            CheckBox cbNewGame = new CheckBox();
+            CheckBox cbOldGame = new CheckBox();
+            HBox createGame = new HBox();
+            createGame.getChildren().addAll(cbNewGame, cbOldGame);
+            createGame.setSpacing(70);
+            createGame.setScaleX(1.2);
+            createGame.setScaleY(1.2);
+            createGame.setPadding(new Insets(0,0,0,27));
+
+            //Game type
+            CheckBox cbClassic = new CheckBox();
+            CheckBox cbAbridged = new CheckBox();
+            HBox gameType = new HBox();
+            gameType.getChildren().addAll(cbAbridged, cbClassic);
+            gameType.setSpacing(90);
+            gameType.setScaleX(1.2);
+            gameType.setScaleY(1.2);
+            gameType.setPadding(new Insets(7,0,0,33));
+
+            //Round time buttons
+            TextField timeLimit = new TextField("");
+            HBox roundTime = new HBox();
+            roundTime.getChildren().add(timeLimit);
+            roundTime.setPadding(new Insets(7,0,0,-15));
+
+            //Number of players buttons
+            TextField noOfPlayers = new TextField();
+            HBox players = new HBox();
+            players.getChildren().add(noOfPlayers);
+            players.setSpacing(100);
+            players.setPadding(new Insets(7,0,0,-15));
+
+            //Add CPU players
+            CheckBox cbAddCpu = new CheckBox();
+            CheckBox cbNoCpu = new CheckBox();
+            HBox CPU = new HBox();
+            CPU.getChildren().addAll(cbAddCpu, cbNoCpu);
+            CPU.setSpacing(70);
+            CPU.setPadding(new Insets(7,0,0,25));
+            CPU.setScaleX(1.2);
+            CPU.setScaleY(1.2);
+
+            //Creating Selection For Player Tokens
+            ComboBox cmbPawn = new ComboBox();
+            Label cbBoot = new Label("Boot");
+            Label cbSmartphone = new Label("Smartphone");
+            Label cbGoblet = new Label("Goblet");
+            Label cbHatstand = new Label("Hatstand");
+            Label cbCat = new Label("Cat");
+            Label cbSpoon = new Label("Spoon");
+            cmbPawn.getItems().addAll(cbBoot, cbSmartphone, cbGoblet, cbHatstand, cbCat, cbSpoon);
+            HBox tokenSelection = new HBox();
+            tokenSelection.getChildren().add(cmbPawn);
+            tokenSelection.setSpacing(100);
+
+
             //Setting Button Menu
             HBox startButtons = new HBox(btnStart, btnSettings, btnHelp, btnExit);
             startButtons.setSpacing(30);
             startButtons.setAlignment(Pos.CENTER);
-
-            //Creating player selction menu
-            Label labBoot = new Label("BOOT");
-            Label labSmartphone = new Label("SMARTPHONE");
-            Label labGoblet = new Label("GOBLET");
-            Label labHatstand = new Label("HATSTAND");
-            Label labCat = new Label("CAT");
-            Label labSpoon = new Label("SPOON");
-
-            CheckBox cbBoot = new CheckBox();
-            CheckBox cbSmartphone = new CheckBox();
-            CheckBox cbGoblet = new CheckBox();
-            CheckBox cbHatstand = new CheckBox();
-            CheckBox cbCat = new CheckBox();
-            CheckBox cbSpoon = new CheckBox();
-
-            //Setting the player selection menu
-            HBox tokLabel = new HBox();
-            tokLabel.getChildren().addAll(labBoot, labSmartphone, labGoblet, labHatstand, labCat, labSpoon);
-            HBox tokCheckbox = new HBox();
-            tokCheckbox.getChildren().addAll(cbBoot, cbSmartphone, cbGoblet, cbHatstand, cbCat, cbSpoon);
-
-            
-
-
+            startButtons.setPadding(new Insets(0, 0, 130, 30));
 
             // Creating the background
-            FileInputStream bc = new FileInputStream("src/main/frontend/resources/bc.jpg");
-            Image image = new Image(bc);
+            FileInputStream bc = new FileInputStream("src/main/java/frontend/resources/website/empty_menu.jpg");
+            Image image = new Image(bc, 1280, 720, true, true);
             BackgroundImage backgroundimage = new BackgroundImage(image,
                     BackgroundRepeat.NO_REPEAT,
                     BackgroundRepeat.NO_REPEAT,
-                    BackgroundPosition.DEFAULT,
+                    BackgroundPosition.CENTER,
                     BackgroundSize.DEFAULT);
             Background background = new Background(backgroundimage);
-            startButtons.setBackground(background);
+
+            //Setting up the scene
+            TilePane main = new TilePane();
+            main.setHgap(1000);
+            main.setVgap(20);
+            main.setPadding(new Insets(243,0,0,600));
+            main.getChildren().add(createGame);
+            main.getChildren().add(gameType);
+            main.getChildren().add(roundTime);
+            main.getChildren().add(players);
+            main.getChildren().add(tokenSelection);
+            main.getChildren().add(CPU);
+
+
 
             BorderPane root = new BorderPane();
+            root.setBackground(background);
+
+            root.setCenter(main);
             root.setBottom(startButtons);
-            Group tokenLabel = new Group(tokLabel);
-            Group tokenCb = new Group(tokCheckbox);
-            root.setCenter(tokenLabel);
+            BorderPane.setMargin(startButtons, new Insets(0));
 
             // create a scene
-            Scene scene = new Scene(root, 652, 800);
+            Scene scene = new Scene(root, 1280, 720);
             // set the scene
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -133,7 +194,7 @@ public class mainMenu extends Application {
         }
     }
 
-    // Main Method
+    // Main Method@
     public static void main(String args[])
     {
         // launch the application
